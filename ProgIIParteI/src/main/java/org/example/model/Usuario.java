ae45a508f852f576799ef901fbf350dd5e8b93d6
Guilherme <185707585+guilhermebt1@users.noreply.github.com>
2026-05-14 13:05:29 -0300

    Model Usuario e Perfil criados e validados

---
+++ ProgIIParteI/src/main/java/org/example/model/Usuario.java
@@ -0,0 +1,68 @@
+package org.example.model;
+
+public class Usuario {
+
+    private int id;
+    private String nome;
+    private String sobrenome;
+    private String login;
+    private String senha;
+    private Perfil perfil;
+
+    public Usuario(int id, String nome, String sobrenome, String login, String senha, Perfil perfil) {
+        this.id = id;
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.login = login;
+        this.senha = senha;
+        this.perfil = perfil;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Perfil getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(Perfil perfil) {
+        this.perfil = perfil;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
