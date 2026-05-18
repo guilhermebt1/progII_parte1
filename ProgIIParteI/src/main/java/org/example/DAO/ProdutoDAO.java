e36cd93defd3d3e783f1a9ad97f8c7b14e0873f0
Guilherme <185707585+guilhermebt1@users.noreply.github.com>
2026-05-18 13:36:20 -0300

    Interface ProdutoDAO implementada

---
+++ ProgIIParteI/src/main/java/org/example/DAO/ProdutoDAO.java
@@ -0,0 +1,14 @@
+package org.example.DAO;
+
+import org.example.model.Produto;
+
+import java.util.List;
+
+public interface ProdutoDAO {
+
+    void cadastrar(Produto produto);
+    Produto buscarPorId(int id);
+    List<Produto> buscarPorNome(String produtoNome);
+    void remover(int id);
+    void atualizar(Produto produto);
+}
