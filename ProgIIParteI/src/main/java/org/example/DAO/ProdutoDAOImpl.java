4a066141e8128164ca1dd0ac66e226e41b4dec18
Guilherme <185707585+guilhermebt1@users.noreply.github.com>
2026-05-18 13:54:22 -0300

    ProdutoDAOImpl finalizado

--- ProgIIParteI/src/main/java/org/example/DAO/ProdutoDAOImpl.java
+++ ProgIIParteI/src/main/java/org/example/DAO/ProdutoDAOImpl.java
@@ -1,4 +1,120 @@
 package org.example.DAO;
 
-public class ProdutoDAOImpl {
+import org.example.conexao.ConnectionFactory;
+import org.example.model.Perfil;
+import org.example.model.Produto;
+import org.example.model.Usuario;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProdutoDAOImpl implements ProdutoDAO {
+
+    @Override
+    public void cadastrar(Produto produto){
+        String sql = "INSERT INTO produtos (nome, descricao, preco, estoque) VALUES (?,?,?,?)";
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)){
+
+            stmt.setString(1, produto.getNome());
+            stmt.setString(2, produto.getDescricao());
+            stmt.setDouble(3, produto.getPreco());
+            stmt.setInt(4, produto.getEstoque());
+            stmt.executeUpdate();
+
+
+        } catch (Exception e){
+            System.out.println("Erro ao cadastrar produto " + e.getMessage());
+        }
+    }
+
+    @Override
+    public Produto buscarPorId(int id){
+        String sql = "SELECT * FROM produtos WHERE id = ?";
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+
+            if(rs.next()) {
+                Produto produto = new Produto(
+                        rs.getInt("id"),
+                        rs.getString("nome"),
+                        rs.getString("descricao"),
+                        rs.getDouble("preco"),
+                        rs.getInt("estoque")
+                );
+                return produto;
+            }
+        } catch (Exception e){
+            System.out.println("Erro ao buscar produto: " + e.getMessage());
+        }
+        return null;
+    }
+
+    @Override
+    public List<Produto> buscarPorNome(String produtoNome) {
+        String sql = "SELECT * FROM produtos WHERE nome LIKE ?";
+        List<Produto> produtos = new ArrayList<>();
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, "%" + produtoNome + "%");
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Produto produto = new Produto(
+                        rs.getInt("id"),
+                        rs.getString("nome"),
+                        rs.getString("descricao"),
+                        rs.getDouble("preco"),
+                        rs.getInt("estoque")
+                );
+                produtos.add(produto);
+            }
+
+        } catch (Exception e) {
+            System.out.println("Erro ao buscar produto: " + e.getMessage());
+        }
+        return produtos;
+    }
+
+    @Override
+    public void remover(int id) {
+        String sql = "DELETE FROM produtos WHERE id = ?";
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+
+        } catch (Exception e) {
+            System.out.println("Erro ao remover produto: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void atualizar(Produto produto) {
+        String sql = "UPDATE produtos SET nome = ?, descricao = ?, preco = ?, estoque = ? WHERE id = ?";
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, produto.getNome());
+            stmt.setString(2, produto.getDescricao());
+            stmt.setDouble(3, produto.getPreco());
+            stmt.setInt(4, produto.getEstoque());
+            stmt.setInt(5, produto.getId());
+            stmt.executeUpdate();
+
+        } catch (Exception e) {
+            System.out.println("Erro ao atualizar produto: " + e.getMessage());
+        }
+    }
+
 }
+
+
